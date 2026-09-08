a3dd6f0131844ad9f8a7bdfc01184b51bf0c5a92
Fady Fawzy <fady.fawzy2006@gmail.com>
2026-09-08 15:47:13 +0300

    Using NP

--- Week 2/FindTheMeanForAList.py
+++ Week 2/FindTheMeanForAList.py
@@ -1,14 +1,20 @@
+import numpy as np
+
+
 def FindMean(numbers):
     if len(numbers) == 0:
         raise ValueError("The list is empty, cannot compute mean.")
     else:
         return sum(numbers) / len(numbers)
 
+numbers = list(map(float, input("Enter numbers separated by spaces: ").split()))
+try:
+    mean = FindMean(numbers)
+    print(f"The mean of the entered numbers (from the function) is: {mean}")
+except ValueError as e:
+    print(e)
 
-for i in range(5):
-    try:
-        numbers = list(map(float, input("Enter numbers separated by spaces: ").split()))
-        mean = FindMean(numbers)
-        print(f"The mean of the entered numbers is: {mean}")
-    except ValueError as e:
-        print(e)
+NpArray = np.array(numbers)
+mean = np.mean(NpArray)
+print(f"The mean of the entered numbers (from NP) is: {mean}")
+
